fe5e3a4733af8d32c0514dc97ef7a188c871433b
Zhou Wei <zhouwei@ehom.net>
2025-10-28 19:13:37 +0800

    feat: enhance VPXDecoder with frame buffer management

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,25 +1,38 @@
-use std::{mem::MaybeUninit, ptr, slice, sync::Arc};
+use std::{
+    mem::MaybeUninit,
+    os::raw::{c_int, c_void},
+    ptr, slice,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
 
 use ctor::ctor;
 use media_codec::{
     codec::{Codec, CodecBuilder, CodecID},
     decoder::{register_decoder, Decoder, DecoderBuilder, VideoDecoder, VideoDecoderParameters},
     packet::Packet,
-    CodecInfomation,
+    CodecInformation, CodecParameters,
 };
-use media_codec_vpx_sys::vpx_codec_iter_t;
 use media_core::{
+    buffer::{Buffer, BufferPool},
     error::Error,
-    frame::Frame,
+    frame::{Frame, SharedFrame},
+    frame_pool::{FrameCreator, FramePool},
     unsupported_error,
     variant::Variant,
     video::{ColorMatrix, ColorRange, PixelFormat, VideoFrameDescriptor},
-    Result,
+    FrameDescriptor, Result,
 };
+use smallvec::SmallVec;
 
 use crate::{
     vpx_error_string,
-    vpx_sys::{self, vpx_codec_ctx_t, vpx_codec_err_t::VPX_CODEC_OK, vpx_color_range, vpx_color_space, vpx_img_fmt, VPX_DECODER_ABI_VERSION},
+    vpx_sys::{
+        self, vpx_codec_ctx_t, vpx_codec_err_t::VPX_CODEC_OK, vpx_codec_frame_buffer_t, vpx_codec_iter_t, vpx_color_range, vpx_color_space,
+        vpx_image_t, vpx_img_fmt, VPX_DECODER_ABI_VERSION,
+    },
 };
 
 fn vpx_img_fmt_to_pixel_format(fmt: vpx_img_fmt, depth: u32) -> Option<PixelFormat> {
@@ -65,18 +78,100 @@ fn vpx_color_space_to_color_matrix(color_space: vpx_color_space) -> ColorMatrix 
     }
 }
 
+const DEFAULT_MAX_VIDEO_PLANES: usize = 4;
+
+type BufferPlaneVec = SmallVec<[(usize, u32); DEFAULT_MAX_VIDEO_PLANES]>;
+
+struct VPXImage(vpx_image_t);
+
+impl VPXImage {
+    fn has_frame_buffer(&self) -> bool {
+        !self.0.fb_priv.is_null()
+    }
+
+    fn descriptor(&self) -> Result<VideoFrameDescriptor> {
+        let img = &self.0;
+        let frame_width = img.d_w;
+        let frame_height = img.d_h;
+        let pix_fmt = img.fmt;
+        let depth = img.bit_depth;
+
+        let pixel_format = vpx_img_fmt_to_pixel_format(pix_fmt, depth).ok_or_else(|| unsupported_error!(pix_fmt))?;
+
+        let mut desc = VideoFrameDescriptor::try_new(pixel_format, frame_width, frame_height)?;
+        desc.color_range = vpx_color_range_to_color_range(img.range);
+        desc.color_matrix = vpx_color_space_to_color_matrix(img.cs);
+
+        Ok(desc)
+    }
+
+    fn convert_to_frame(&self) -> Result<Frame<'_>> {
+        let img = &self.0;
+        let desc = self.descriptor()?;
+        let planes_num = desc.format.components() as usize;
+        let mut buffers = SmallVec::<[(&[u8], u32); DEFAULT_MAX_VIDEO_PLANES]>::with_capacity(planes_num);
+
+        for plane in 0..planes_num {
+            let height = desc.format.calc_plane_height(plane, desc.height.get()) as usize;
+            let stride = img.stride[plane] as usize;
+            let buffer = unsafe { slice::from_raw_parts(img.planes[plane], stride * height) };
+            buffers.push((buffer, stride as u32));
+        }
+
+        let frame = Frame::video_creator().create_from_buffers_with_descriptor(desc, &buffers)?;
+
+        Ok(frame)
+    }
+
+    fn convert_to_buffer(&self) -> Result<(Arc<Buffer>, BufferPlaneVec, VideoFrameDescriptor)> {
+        let img = &self.0;
+        let desc = self.descriptor()?;
+        let planes_num = desc.format.components() as usize;
+        let buffer = unsafe { Arc::from_raw(img.fb_priv as *const Buffer) };
+
+        let mut buffers = SmallVec::with_capacity(planes_num);
+
+        for plane in 0..planes_num {
+            let offset = img.planes[plane] as usize - buffer.data().as_ptr() as usize;
+
+            if offset >= buffer.len() {
+                let _ = Arc::into_raw(buffer);
+                return Err(Error::Invalid("invalid frame buffer offset".to_string()));
+            }
+
+            let stride = img.stride[plane] as usize;
+            buffers.push((offset, stride as u32));
+        }
+
+        let buffer_clone = buffer.clone();
+        let _ = Arc::into_raw(buffer);
+
+        Ok((buffer_clone, buffers, desc))
+    }
+}
+
+struct EmptyFrameCreator;
+
+impl FrameCreator for EmptyFrameCreator {
+    fn create_frame(&self, desc: FrameDescriptor) -> Result<Frame<'static>> {
+        Frame::video_creator().create_empty_with_descriptor(desc.try_into()?)
+    }
+}
+
 pub struct VPXDecoder {
     id: CodecID,
     name: &'static str,
     ctx: vpx_codec_ctx_t,
     iter: vpx_codec_iter_t,
+    buffer_pool_ptr: *const BufferPool,
+    frame_pool_initialized: AtomicBool,
 }
 
 unsafe impl Send for VPXDecoder {}
 unsafe impl Sync for VPXDecoder {}
 
 impl Codec<VideoDecoder> for VPXDecoder {
-    fn configure(&mut self, _parameters: Option<&VideoDecoderParameters>, _options: Option<&Variant>) -> Result<()> {
+    fn configure(&mut self, _params: Option<&CodecParameters>, _options: Option<&Variant>) -> Result<()> {
         Ok(())
     }
 
@@ -86,7 +181,7 @@ impl Codec<VideoDecoder> for VPXDecoder {
 }
 
 impl Decoder<VideoDecoder> for VPXDecoder {
-    fn send_packet(&mut self, _config: &VideoDecoder, packet: &Packet) -> Result<()> {
+    fn send_packet(&mut self, _config: &VideoDecoder, _pool: Option<&Arc<FramePool<Frame<'static>>>>, packet: Packet) -> Result<()> {
         let packet_data = packet.data();
         let ret = unsafe { vpx_sys::vpx_codec_decode(&mut self.ctx, packet_data.as_ptr(), packet_data.len() as u32, ptr::null_mut(), 0) };
 
@@ -99,37 +194,52 @@ impl Decoder<VideoDecoder> for VPXDecoder {
         Ok(())
     }
 
+    fn receive_frame(&mut self, _config: &VideoDecoder, pool: Option<&Arc<FramePool<Frame<'static>>>>) -> Result<SharedFrame<Frame<'static>>> {
+        let img = &self.get_image()?;
+
+        let pool = if let Some(pool) = pool {
+            pool
+        } else {
+            if !img.has_frame_buffer() {
+                return img.convert_to_frame().map(SharedFrame::<Frame<'static>>::new);
+            }
+
+            let (buffer, buffer_planes, desc) = img.convert_to_buffer()?;
+            let frame = Frame::video_creator().create_from_shared_buffer_with_descriptor(desc, buffer, &buffer_planes)?;
+
+            return Ok(SharedFrame::<Frame<'static>>::new(frame));
+        };
+
+        if !img.has_frame_buffer() {
+            let desc = img.descriptor()?;
+
+            if !self.frame_pool_initialized.load(Ordering::Relaxed) {
+                pool.configure(Some(desc.clone().into()), None);
+                self.frame_pool_initialized.store(true, Ordering::Relaxed);
+            }
+
+            let frame = img.convert_to_frame()?;
+            let mut pooled_frame = pool.get_frame_with_descriptor(desc.into())?;
+            frame.convert_to(pooled_frame.write().unwrap())?;
+
+            Ok(pooled_frame)
+        } else {
+            let (buffer, buffer_planes, desc) = img.convert_to_buffer()?;
+
+            if !self.frame_pool_initialized.load(Ordering::Relaxed) {
+                pool.configure(Some(desc.clone().into()), Some(Box::new(EmptyFrameCreator)));
+                self.frame_pool_initialized.store(true, Ordering::Relaxed);
+            }
+
+            let mut pooled_frame = pool.get_frame_with_descriptor(desc.clone().into())?;
+            pooled_frame.write().unwrap().attach_video_shared_buffer_with_descriptor(desc, buffer, &buffer_planes)?;
+
+            Ok(pooled_frame)
+        }
+    }
+
     fn receive_frame_borrowed(&mut self, _config: &VideoDecoder) -> Result<Frame<'_>> {
-        let img = unsafe { vpx_sys::vpx_codec_get_frame(&mut self.ctx, &mut self.iter) };
-        if img.is_null() {
-            return Err(Error::Again("no frame available".to_string()));
-        }
-
-        let img_ref = unsafe { &*img };
-        let frame_width = img_ref.d_w;
-        let frame_height = img_ref.d_h;
-        let pix_fmt = img_ref.fmt;
-        let depth = img_ref.bit_depth;
-
-        let pixel_format = vpx_img_fmt_to_pixel_format(pix_fmt, depth).ok_or_else(|| unsupported_error!(pix_fmt))?;
-
-        let planes = pixel_format.components() as usize;
-        let mut buffers = Vec::with_capacity(planes);
-
-        for plane in 0..planes {
-            let height = pixel_format.calc_plane_height(plane, frame_height) as usize;
-            let stride = img_ref.stride[plane] as usize;
-            let buffer = unsafe { slice::from_raw_parts(img_ref.planes[plane], stride * height) };
-            buffers.push((buffer, stride as u32));
-        }
-
-        let mut desc = VideoFrameDescriptor::try_new(pixel_format, frame_width, frame_height)?;
-        desc.color_range = vpx_color_range_to_color_range(img_ref.range);
-        desc.color_matrix = vpx_color_space_to_color_matrix(img_ref.cs);
-
-        let frame = Frame::video_creator().create_from_buffers_with_descriptor(desc, &buffers)?;
-
-        Ok(frame)
+        Err(unsupported_error!("borrowed frame"))
     }
 
     fn flush(&mut self, _config: &VideoDecoder) -> Result<()> {
@@ -145,14 +255,35 @@ impl Decoder<VideoDecoder> for VPXDecoder {
     }
 }
 
-impl Drop for VPXDecoder {
-    fn drop(&mut self) {
-        unsafe { vpx_sys::vpx_codec_destroy(&mut self.ctx) };
+unsafe extern "C" fn get_frame_buffer(priv_: *mut c_void, min_size: usize, fb: *mut vpx_codec_frame_buffer_t) -> c_int {
+    let pool = Arc::from_raw(priv_ as *const BufferPool);
+
+    if pool.get_buffer_capacity() < min_size {
+        pool.set_buffer_capacity(min_size);
     }
+
+    let buffer = pool.get_buffer();
+
+    (*fb).data = buffer.data().as_ptr() as *mut u8;
+    (*fb).size = buffer.len();
+    (*fb).priv_ = Arc::into_raw(buffer) as *mut c_void;
+
+    let _ = Arc::into_raw(pool);
+
+    0
+}
+
+unsafe extern "C" fn release_frame_buffer(_priv_: *mut c_void, fb: *mut vpx_codec_frame_buffer_t) -> c_int {
+    if !(*fb).priv_.is_null() {
+        let buffer = Arc::from_raw((*fb).priv_ as *const Buffer);
+        drop(buffer);
+    }
+
+    0
 }
 
 impl VPXDecoder {
-    pub fn new(id: CodecID, _parameters: &VideoDecoderParameters, _options: Option<&Variant>) -> Result<Self> {
+    pub fn new(id: CodecID, _params: &VideoDecoderParameters, _options: Option<&Variant>) -> Result<Self> {
         let (iface, name) = match id {
             CodecID::VP8 => (unsafe { vpx_sys::vpx_codec_vp8_dx() }, VP8_CODEC_NAME),
             CodecID::VP9 => (unsafe { vpx_sys::vpx_codec_vp9_dx() }, VP9_CODEC_NAME),
@@ -168,12 +299,52 @@ impl VPXDecoder {
             return Err(Error::Invalid(vpx_error_string(ret)));
         }
 
+        let pool = BufferPool::new(0);
+        let pool_ptr = Arc::into_raw(pool);
+
+        if id == CodecID::VP9 {
+            unsafe {
+                vpx_sys::vpx_codec_set_frame_buffer_functions(
+                    ctx.as_mut_ptr(),
+                    Some(get_frame_buffer),
+                    Some(release_frame_buffer),
+                    pool_ptr as *mut c_void,
+                );
+            }
+        }
+
         Ok(Self {
             id,
             name,
             ctx: unsafe { ctx.assume_init() },
             iter: ptr::null_mut(),
+            buffer_pool_ptr: pool_ptr,
+            frame_pool_initialized: AtomicBool::new(false),
         })
+    }
+
+    fn get_image(&mut self) -> Result<VPXImage> {
+        let img = unsafe { vpx_sys::vpx_codec_get_frame(&mut self.ctx as *const _ as *mut _, &mut self.iter) };
+        if img.is_null() {
+            return Err(Error::Again("no frame available".to_string()));
+        }
+
+        let img = unsafe { *img };
+
+        Ok(VPXImage(img))
+    }
+}
+
+impl Drop for VPXDecoder {
+    fn drop(&mut self) {
+        unsafe {
+            vpx_sys::vpx_codec_destroy(&mut self.ctx);
+
+            if !self.buffer_pool_ptr.is_null() {
+                let pool = Arc::from_raw(self.buffer_pool_ptr);
+                drop(pool);
+            }
+        };
     }
 }
 
@@ -183,13 +354,8 @@ pub struct VPXDecoderBuilder {
 }
 
 impl DecoderBuilder<VideoDecoder> for VPXDecoderBuilder {
-    fn new_decoder(
-        &self,
-        codec_id: CodecID,
-        parameters: &VideoDecoderParameters,
-        options: Option<&Variant>,
-    ) -> Result<Box<dyn Decoder<VideoDecoder>>> {
-        Ok(Box::new(VPXDecoder::new(codec_id, parameters, options)?))
+    fn new_decoder(&self, codec_id: CodecID, params: &CodecParameters, options: Option<&Variant>) -> Result<Box<dyn Decoder<VideoDecoder>>> {
+        Ok(Box::new(VPXDecoder::new(codec_id, &params.try_into()?, options)?))
     }
 }
 
@@ -203,7 +369,7 @@ impl CodecBuilder<VideoDecoder> for VPXDecoderBuilder {
     }
 }
 
-impl CodecInfomation for VPXDecoder {
+impl CodecInformation for VPXDecoder {
     fn id(&self) -> CodecID {
         self.id
     }
